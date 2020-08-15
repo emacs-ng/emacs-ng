8792e56e84e21256aecdf7074d911130a3712e27
Harry Fei <tiziyuanfang@gmail.com>
2020-08-15 21:09:53 +0800

    webrender: speedup keyboard intput

--- rust_src/src/webrender_backend/term.rs
+++ rust_src/src/webrender_backend/term.rs
@@ -156,6 +156,11 @@ extern "C" fn update_window_end(
         unsafe { gui_draw_vertical_border(window.as_mut()) }
     }
     unsafe { unblock_input() };
+
+    let frame: LispFrameRef = window.get_frame();
+    let mut output: OutputRef = unsafe { frame.output_data.wr.into() };
+
+    output.flush();
 }
 
 extern "C" fn flush_display(f: *mut Lisp_Frame) {
