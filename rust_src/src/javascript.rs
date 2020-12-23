17b1e96f988b3dbcdd9a9ce698ef84083f8bd446
David DeSimone <lord.good.mail@gmail.com>
2020-12-23 15:15:06 -0500

    Adding returnvalue

--- rust_src/src/javascript.rs
+++ rust_src/src/javascript.rs
@@ -1,9 +1,10 @@
 use crate::lisp::LispObject;
 use crate::multibyte::LispStringRef;
-use crate::remacs_sys::make_string_from_utf8;
+use crate::remacs_sys::{intern_c_string, make_string_from_utf8, Ffuncall};
 use lazy_static::lazy_static;
 use remacs_macros::lisp_fn;
 use rusty_v8 as v8;
+use std::convert::TryFrom;
 use std::convert::TryInto;
 use std::ffi::CString;
 
@@ -31,6 +32,53 @@ impl EmacsIsolate {
     }
 }
 
+/*
+var lisp = new Proxy({}, {
+  get: function(target, key) {
+    return function() {
+      _lisp_funcall.apply(key, arguments);
+    }
+  }
+});
+
+in Rust
+fn _lisp_funcall(key: String, args: Vec<_>) {
+    let func = intern!(key);
+    let result = ffuncall(key, args);
+    return result;
+}
+
+ */
+
+pub fn lisp_callback(
+    scope: &mut v8::HandleScope,
+    args: v8::FunctionCallbackArguments,
+    mut retval: v8::ReturnValue,
+) {
+    let message = args
+        .get(0)
+        .to_string(scope)
+        .unwrap()
+        .to_rust_string_lossy(scope);
+
+    let lisp_args = args
+        .get(1)
+        .to_string(scope)
+        .unwrap()
+        .to_rust_string_lossy(scope);
+
+    let cstr = CString::new(message).expect("Failure of CString");
+    let interned = unsafe { intern_c_string(cstr.as_ptr()) };
+    let mut args = vec![interned];
+    let results = unsafe { Ffuncall(args.len().try_into().unwrap(), args.as_mut_ptr()) };
+
+    //    let json = crate::parsing::json_se(&[results]);
+    let string_o: LispStringRef = results.into();
+    let string = string_o.to_utf8();
+    let r = v8::Local::<v8::Value>::try_from(v8::String::new(scope, &string).unwrap()).unwrap();
+    retval.set(r);
+}
+
 #[lisp_fn]
 pub fn eval_js(string_obj: LispStringRef) -> LispObject {
     let isolate = MAIN.isolate();
@@ -38,8 +86,13 @@ pub fn eval_js(string_obj: LispStringRef) -> LispObject {
     // Create a stack-allocated handle scope.
     let handle_scope = &mut v8::HandleScope::new(isolate);
 
-    // Create a new context.
-    let context = v8::Context::new(handle_scope);
+    let global = v8::ObjectTemplate::new(handle_scope);
+    global.set(
+        v8::String::new(handle_scope, "lisp_invoke").unwrap().into(),
+        v8::FunctionTemplate::new(handle_scope, lisp_callback).into(),
+    );
+
+    let context = v8::Context::new_from_template(handle_scope, global);
 
     // Enter the context for compiling and running the hello world script.
     let scope = &mut v8::ContextScope::new(handle_scope, context);
