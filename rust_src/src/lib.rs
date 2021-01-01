fangyuanziti/remacs

webrender: implement `x-open-connection` and `x-create-frame` basic logic

rust_src/src/lib.rs
@@ -126,6 +126,8 @@ mod threads;
 mod time;
 mod util;
 mod vectors;
+#[cfg(feature = "window-system-webrender")]
+mod webrender;
 mod window_configuration;
 mod windows;
 #[cfg(feature = "window-system-webrender")]

emacs-ng/emacs-ng

rust_src/src/lib.rs
@@ -57,6 +57,9 @@ mod multibyte;
 mod process;
 mod vectors;
 #[cfg(feature = "window-system-webrender")]
+mod webrender_backend;
+mod windows;
+#[cfg(feature = "window-system-webrender")]
 mod wrterm;
 
 #[cfg(feature = "window-system-webrender")]