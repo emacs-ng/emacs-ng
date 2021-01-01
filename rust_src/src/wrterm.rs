fangyuanziti/remacs

webrender: enable x term feature

rust_src/src/wrterm.rs
@@ -9,8 +9,9 @@ use crate::{
     frame::LispFrameRef,
     lisp::{ExternalPtr, LispObject},
     remacs_sys::{
-        font, wr_display_info, wr_output, Display, Pixmap, Qnil, WRImage, Window, XColor,
-        XrmDatabase,
+        font, hashtest_eql, make_hash_table, wr_display_info, wr_output, Display, Fprovide, Pixmap,
+        Qnil, Qt, Qwr, Qx, WRImage, Window, XColor, XrmDatabase, DEFAULT_REHASH_SIZE,
+        DEFAULT_REHASH_THRESHOLD,
     },
 };
 
@@ -167,9 +168,52 @@ pub fn x_hide_tip() -> bool {
     unimplemented!();
 }
 
+/// Make a new X window, which is called a "frame" in Emacs terms.
+/// Return an Emacs frame object.  PARMS is an alist of frame parameters.
+/// If the parameters specify that the frame should not have a minibuffer,
+/// and do not specify a specific minibuffer window to use, then
+/// `default-minibuffer-frame' must be a frame whose minibuffer can be
+/// shared by the new frame.
+///
+/// This function is an internal primitive--use `make-frame' instead.
+#[lisp_fn]
+pub fn x_create_frame(_parms: LispObject) {
+    unimplemented!();
+}
+
 #[no_mangle]
+#[allow(unused_doc_comments)]
 pub extern "C" fn syms_of_wrterm() {
+    // pretend webrender as a X gui backend, so we can reuse the x-win.el logic
+    def_lisp_sym!(Qx, "x");
     def_lisp_sym!(Qwr, "wr");
+    unsafe {
+        Fprovide(Qx, Qnil);
+        Fprovide(Qwr, Qnil);
+    }
+
+    let x_keysym_table = unsafe {
+        make_hash_table(
+            hashtest_eql.clone(),
+            900,
+            DEFAULT_REHASH_SIZE,
+            DEFAULT_REHASH_THRESHOLD,
+            Qnil,
+            false,
+        )
+    };
+
+    /// Hash table of character codes indexed by X keysym codes.
+    #[rustfmt::skip]
+    defvar_lisp!(Vx_keysym_table, "x-keysym-table", x_keysym_table);
+
+    /// Which toolkit scroll bars Emacs uses, if any.
+    /// A value of nil means Emacs doesn't use toolkit scroll bars.
+    /// With the X Window system, the value is a symbol describing the
+    /// X toolkit.  Possible values are: gtk, motif, xaw, or xaw3d.
+    /// With MS Windows or Nextstep, the value is t.
+    #[rustfmt::skip]
+    defvar_lisp!(Vx_toolkit_scroll_bars, "x-toolkit-scroll-bars", Qt);
 }
 
 include!(concat!(env!("OUT_DIR"), "/wrterm_exports.rs"));

src/dispnew.c
@@ -5831,6 +5831,15 @@ init_display (void)
     }
 #endif
 
+#ifdef USE_WEBRENDER
+  if (!inhibit_window_system)
+    {
+      Vinitial_window_system = Qx; /* pretend webrender as a X gui backend, so we can reuse the x-win.el logic */
+      Vwindow_system_version = make_number (1);
+      return;
+    }
+#endif /* USE_WEBRENDER */
+
   /* If no window system has been specified, try to use the terminal.  */
   if (! isatty (STDIN_FILENO))
     fatal ("standard input is not a tty");

emacs-ng/emacs-ng

rust_src/src/wrterm.rs
@@ -8,8 +8,9 @@ use crate::{
     frame::LispFrameRef,
     lisp::{ExternalPtr, LispObject},
     remacs_sys::{
-        font, image, wr_display_info, wr_output, Display, Emacs_Pixmap, Pixmap, Qnil, WRImage,
-        Window, XrmDatabase,
+        font, hashtest_eql, image, make_hash_table, wr_display_info, wr_output, Display,
+        Emacs_Pixmap, Fprovide, Pixmap, Qnil, Qwr, Qx, WRImage, Window, XrmDatabase,
+        DEFAULT_REHASH_SIZE, DEFAULT_REHASH_THRESHOLD,
     },
 };
 
@@ -164,9 +165,69 @@ pub fn x_hide_tip() -> bool {
     unimplemented!();
 }
 
+/// Make a new X window, which is called a "frame" in Emacs terms.
+/// Return an Emacs frame object.  PARMS is an alist of frame parameters.
+/// If the parameters specify that the frame should not have a minibuffer,
+/// and do not specify a specific minibuffer window to use, then
+/// `default-minibuffer-frame' must be a frame whose minibuffer can be
+/// shared by the new frame.
+///
+/// This function is an internal primitive--use `make-frame' instead.
+#[lisp_fn]
+pub fn x_create_frame(_parms: LispObject) {
+    unimplemented!();
+}
+
 #[no_mangle]
+#[allow(unused_doc_comments)]
 pub extern "C" fn syms_of_wrterm() {
+    // pretend webrender as a X gui backend, so we can reuse the x-win.el logic
+    def_lisp_sym!(Qx, "x");
     def_lisp_sym!(Qwr, "wr");
+    unsafe {
+        Fprovide(Qx, Qnil);
+        Fprovide(Qwr, Qnil);
+    }
+
+    let x_keysym_table = unsafe {
+        make_hash_table(
+            hashtest_eql.clone(),
+            900,
+            DEFAULT_REHASH_SIZE,
+            DEFAULT_REHASH_THRESHOLD,
+            Qnil,
+            false,
+        )
+    };
+
+    // Hash table of character codes indexed by X keysym codes.
+    #[rustfmt::skip]
+    defvar_lisp!(Vx_keysym_table, "x-keysym-table", x_keysym_table);
+
+    // Which toolkit scroll bars Emacs uses, if any.
+    // A value of nil means Emacs doesn't use toolkit scroll bars.
+    // With the X Window system, the value is a symbol describing the
+    // X toolkit.  Possible values are: gtk, motif, xaw, or xaw3d.
+    // With MS Windows or Nextstep, the value is t.
+    #[rustfmt::skip]
+    defvar_lisp!(Vx_toolkit_scroll_bars, "x-toolkit-scroll-bars", Qnil);
+
+    // Non-nil means make use of UNDERLINE_POSITION font properties.
+    // A value of nil means ignore them.  If you encounter fonts with bogus
+    // UNDERLINE_POSITION font properties, set this to nil.  You can also use
+    // `underline-minimum-offset' to override the font's UNDERLINE_POSITION for
+    // small font display sizes.
+    #[rustfmt::skip]
+    defvar_bool!(Vx_use_underline_position_properties, "x-use-underline-position-properties", true);
+
+    // Non-nil means to draw the underline at the same place as the descent line.
+    // (If `line-spacing' is in effect, that moves the underline lower by
+    // that many pixels.)
+    // A value of nil means to draw the underline according to the value of the
+    // variable `x-use-underline-position-properties', which is usually at the
+    // baseline level.  The default value is nil.
+    #[rustfmt::skip]
+    defvar_bool!(Vx_underline_at_descent_line, "x-underline-at-descent-line", false);
 }
 
 include!(concat!(env!("OUT_DIR"), "/wrterm_exports.rs"));

src/dispnew.c
@@ -6403,6 +6403,15 @@ init_display_interactive (void)
     }
 #endif
 
+#ifdef USE_WEBRENDER
+  if (!inhibit_window_system)
+    {
+      Vinitial_window_system = Qx; /* pretend webrender as a X gui backend, so we can reuse the x-win.el logic */
+      Vwindow_system_version = make_fixnum (1);
+      return;
+    }
+#endif /* USE_WEBRENDER */
+
   /* If no window system has been specified, try to use the terminal.  */
   if (! isatty (STDIN_FILENO))
     fatal ("standard input is not a tty");