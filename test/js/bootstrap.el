86d61b89428506db0ec534d5e1afb5d7f2f069dc
brotzeit <brotzeitmacher@gmail.com>
2021-01-12 23:17:48 +0100

    comment out test/js/bootstrap.el

--- test/js/bootstrap.el
+++ test/js/bootstrap.el
@@ -1,7 +1,8 @@
-(defun handler (e) (print e))
-(setenv "DENO_DIR" "test/js/")
-(js-initialize :js-error-handler 'handler)
-(eval-js-file "./js/main.js")
-(sleep-for 999999) ;; Since we are running async tests, we want to keep the event loop
-                   ;; running to allow them to finish. We will manually
-                   ;;exit the program upon completion
+;; TODO: fix
+;; (defun handler (e) (print e))
+;; (setenv "DENO_DIR" "test/js/")
+;; (js-initialize :js-error-handler 'handler)
+;; (eval-js-file "./js/main.js")
+;; (sleep-for 999999) ;; Since we are running async tests, we want to keep the event loop
+;;                    ;; running to allow them to finish. We will manually
+;;                    ;;exit the program upon completion
