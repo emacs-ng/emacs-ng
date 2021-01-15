cd2ded3017d837591548ee325196c60cfe53991a
David DeSimone <lord.good.mail@gmail.com>
2021-01-15 07:18:27 -0800

    Fixing js runtime tests

--- test/js/bootstrap.el
+++ test/js/bootstrap.el
@@ -1,8 +1,12 @@
-;; TODO: fix
-;; (defun handler (e) (print e))
-;; (setenv "DENO_DIR" "test/js/")
-;; (js-initialize :js-error-handler 'handler)
-;; (eval-js-file "./js/main.js")
-;; (sleep-for 999999) ;; Since we are running async tests, we want to keep the event loop
-;;                    ;; running to allow them to finish. We will manually
-;;                    ;;exit the program upon completion
+(defun handler (e) (print e))
+(setenv "DENO_DIR" "test/js/")
+(js-initialize :js-error-handler 'handler)
+(eval-js-file "./js/main.js")
+;; Since we are in batch
+;; manually tick the event
+;; loop
+(run-with-timer t 0.1 'js-tick-event-loop 'handler)
+;; Since we are running async tests, we want to keep the event loop
+;; running to allow them to finish. We will manually
+;; exit the program upon completion
+(sleep-for 999999)
