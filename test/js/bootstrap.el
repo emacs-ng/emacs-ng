be7169a010de69f8c2d940e1b4409820e31644f5
David DeSimone <lord.good.mail@gmail.com>
2021-02-13 15:55:14 -0800

    Fixing bug where JavaScript would not tick the event loop in batch mode

--- test/js/bootstrap.el
+++ test/js/bootstrap.el
@@ -2,10 +2,6 @@
 (setenv "DENO_DIR" "test/js/")
 (js-initialize :js-error-handler 'handler)
 (eval-js-file "./js/main.js")
-;; Since we are in batch
-;; manually tick the event
-;; loop
-(run-with-timer t 0.1 'js-tick-event-loop 'handler)
 ;; Since we are running async tests, we want to keep the event loop
 ;; running to allow them to finish. We will manually
 ;; exit the program upon completion
