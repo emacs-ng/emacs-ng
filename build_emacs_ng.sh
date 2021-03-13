a653ae22febf1a14533c632f9f88ab890a44db04
Kien Nguyen <kien.n.quang@gmail.com>
2021-03-14 03:05:47 +0900

    ci: auto generate release deb package

---
+++ build_emacs_ng.sh
@@ -0,0 +1,24 @@
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/ppa
+sudo apt install -y autoconf make checkinstall texinfo libxpm-dev libjpeg-dev \
+     libgtk-3-dev libgif-dev libtiff-dev libpng-dev libgnutls28-dev libncurses5-dev \
+     libsystemd-dev libjansson-dev libharfbuzz-dev libgccjit-10-dev gcc-10 g++-10
+sudo apt update
+sudo apt -y upgrade
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 10
+
+./autogen.sh
+./configure CFLAGS="-Wl,-rpath,shared -Wl,--disable-new-dtags" \
+            --with-json --with-modules --with-harfbuzz --with-compress-install \
+            --with-threads --with-included-regex --with-zlib --with-cairo --with-libsystemd \
+             --with-nativecomp \
+            --without-rsvg --without-sound --without-imagemagick --without-makeinfo \
+            --without-gpm --without-dbus --without-pop --without-toolkit-scroll-bars \
+            --without-mailutils --without-gsettings \
+            --with-all
+sudo make NATIVE_FULL_AOT=1 PATH=$PATH:$HOME/.cargo/bin -j$(nproc)
+sudo checkinstall -y -D --pkgname=emacs-ng --pkgversion="$1" \
+     --requires="libgif-dev,libjansson-dev,libharfbuzz-dev,libgtk-3-dev,libncurses5-dev,libgccjit-10-dev" \
+     --pkggroup=emacs --gzman=yes --install=no\
+     make install-strip
