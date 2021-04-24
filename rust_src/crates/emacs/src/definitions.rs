5e8f56472ce10747e2421e8c9364d92ce7f58a27
brotzeit <brotzeitmacher@gmail.com>
2021-04-24 15:10:05 +0200

    ignore files generated by bindgen again

--- rust_src/crates/emacs/src/definitions.rs
+++
@@ -1,8 +0,0 @@
-pub type EmacsInt = libc::c_long;
-pub type EmacsUint = libc::c_ulong;
-pub const EMACS_INT_MAX: EmacsInt = 0x7FFFFFFFFFFFFFFF_i64;
-pub const EMACS_INT_SIZE: EmacsInt = 8;
-pub type EmacsDouble = f64;
-pub const EMACS_FLOAT_SIZE: EmacsInt = 8;
-pub type BoolBF = bool;
-pub const USE_LSB_TAG: bool = true;
