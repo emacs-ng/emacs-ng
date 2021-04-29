fa057dbd87223bf8fa9d8e7194c1b1f193994217
David DeSimone <lord.good.mail@gmail.com>
2021-04-28 19:03:20 -0700

    Updating rproxy test

--- test/js/advanced.js
+++ test/js/advanced.js
@@ -2,15 +2,17 @@ Deno.test({
   name: "reverseProxy",
   fn: () => {
     let b = [[555]];
-    let x = lisp.make.proxy({ x: 23, y: b, z: { x: 25 } });
+    let obj = { x: 23, y: b, z: { x: 25 } };
 
     lisp.defun({
       name: "testReverseProxy",
       func: (item) => {
-        if (item !== x) {
+        if (item !== obj) {
           throw new Error("Reverse Proxy failed...");
         }
       },
     });
+
+    lisp.testReverseProxy(lisp.make.proxy(obj));
   },
 });
