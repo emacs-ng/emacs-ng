a8f60f2ade12ccacee09436836d4a2b3d67f3c0c
jman <jman@city17.xyz>
2021-05-01 12:29:06 +0200

    Improve script to generate website documentation
    - the script makes MkDocs serve the content and live reload on changes
    - the Docker container exposes MkDocs http port

--- generate-docs.sh
+++ generate-docs.sh
@@ -4,6 +4,7 @@
 # you can install it with `pip install mkdocs`
 # and then run `mkdocs build`, it will load `mkdocs.yml`
 # generated HTML documentation will be found in `./site`
+# connect your browser to http://localhost:8000
 
 # or using a Docker container:
 # - create a Github token with one permission: read-packages
@@ -14,5 +15,5 @@ GITHUB_ACTOR=YOUR_GITHUB_USERNAME
 GITHUB_TOKEN=${GITHUB_TOKEN}
 
 cp -rf images README.md docs
-docker login docker.pkg.github.com --username $GITHUB_ACTOR --password $GITHUB_TOKEN
-docker run --rm -v ${PWD}:/docs docker.pkg.github.com/emacs-ng/docs-image/docs-image -- build
+#docker login docker.pkg.github.com --username $GITHUB_ACTOR --password $GITHUB_TOKEN
+docker run --network host --rm -v ${PWD}:/docs docker.pkg.github.com/emacs-ng/docs-image/docs-image -- serve
