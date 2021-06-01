f5df400d374d86f95b4d04d267b0d08e8e2605b8
brotzeit <brotzeitmacher@gmail.com>
2021-06-01 23:05:57 +0200

    only call generate_rgb_list when window-system-webrender

--- rust_src/lib.rs
+++ rust_src/lib.rs
@@ -1,5 +1,5 @@
 // Include the main c_exports file that holds the main rust_init_syms.
-// This function calls the other crates init_syms functions which contains
+// This function calls the other crates init_syms functions which contain
 // the generated bindings.
 #[cfg(not(test))]
 include!(concat!(env!("OUT_DIR"), "/c_exports.rs"));
