2443eb3cb74bbd93f0803d569b76f13772c4ce42
Harry Fei <tiziyuanfang@gmail.com>
2021-06-04 18:15:52 +0800

    webrender: fix pbm background and foreground color

--- rust_src/crates/webrender/src/image.rs
+++ rust_src/crates/webrender/src/image.rs
@@ -10,17 +10,26 @@ use emacs::{
     bindings::{add_to_log, image as Emacs_Image, make_float, Fplist_get},
     definitions::EmacsInt,
     frame::LispFrameRef,
-    globals::{QCindex, Qcount, Qdelay, Qgif, Qjpeg, Qnative_image, Qnil, Qpbm, Qpng, Qtiff},
+    globals::{
+        QCbackground, QCforeground, QCindex, Qcount, Qdelay, Qgif, Qjpeg, Qnative_image, Qnil,
+        Qpbm, Qpng, Qtiff,
+    },
     lisp::LispObject,
 };
 use image::{
-    codecs::gif::GifDecoder, imageops::FilterType, io::Reader, AnimationDecoder, DynamicImage,
-    GenericImageView, ImageFormat, ImageResult,
+    codecs::gif::GifDecoder,
+    imageops::FilterType,
+    io::Reader,
+    pnm::{PNMSubtype, PnmDecoder},
+    AnimationDecoder, DynamicImage, GenericImageView, ImageFormat, ImageResult, Rgba,
 };
 use libc::c_void;
-use webrender::api::ImageKey;
+use webrender::api::{ColorF, ColorU, ImageKey};
 
-use super::output::OutputRef;
+use super::{
+    color::{lookup_color_by_name_or_hex, pixel_to_color},
+    output::OutputRef,
+};
 
 pub struct WrPixmap {
     pub image_key: ImageKey,
@@ -38,13 +47,15 @@ fn open_image(
     spec_file: LispObject,
     spec_data: LispObject,
     frame_index: usize,
+    foreground_color: Rgba<u8>,
+    background_color: Rgba<u8>,
 ) -> Option<(DynamicImage, Option<(usize, Duration)>)> {
     if spec_file.is_string() {
         let filename = spec_file.as_string().unwrap().to_string();
 
-        let loaded_image = Reader::open(filename)
-            .ok()
-            .and_then(|r| decode_image_from_reader(r, frame_index).ok());
+        let loaded_image = Reader::open(filename).ok().and_then(|r| {
+            decode_image_from_reader(r, frame_index, foreground_color, background_color).ok()
+        });
 
         return loaded_image;
     }
@@ -53,7 +64,8 @@ fn open_image(
         let data = spec_data.as_string().unwrap();
 
         let reader = Reader::new(Cursor::new(data.as_slice()));
-        let loaded_image = decode_image_from_reader(reader, frame_index).ok();
+        let loaded_image =
+            decode_image_from_reader(reader, frame_index, foreground_color, background_color).ok();
         return loaded_image;
     }
 
@@ -78,16 +90,59 @@ fn decode_gif_image_from_reader<R: BufRead + Seek>(
     ))
 }
 
+fn decode_pnm_image_from_reader<R: BufRead + Seek>(
+    reader: R,
+    foreground_color: Rgba<u8>,
+    background_color: Rgba<u8>,
+) -> ImageResult<DynamicImage> {
+    let pnm_decoder = PnmDecoder::new(reader)?;
+
+    let pnm_type = pnm_decoder.subtype();
+
+    let image = DynamicImage::from_decoder(pnm_decoder)?;
+
+    let black_pixel = Rgba([0, 0, 0, 255]);
+    let white_pixel = Rgba([255, 255, 255, 255]);
+
+    match pnm_type {
+        PNMSubtype::Bitmap(_) => {
+            // Apply foreground and background to mono PBM images.
+            let mut rgba = image.into_rgba8();
+
+            rgba.pixels_mut().for_each(|p| {
+                if *p == black_pixel {
+                    *p = foreground_color;
+                } else if *p == white_pixel {
+                    *p = background_color;
+                }
+            });
+
+            Ok(DynamicImage::ImageRgba8(rgba))
+        }
+        _ => Ok(image),
+    }
+}
+
 fn decode_image_from_reader<R: BufRead + Seek>(
     reader: image::io::Reader<R>,
     frame_index: usize,
+    foreground_color: Rgba<u8>,
+    background_color: Rgba<u8>,
 ) -> ImageResult<(DynamicImage, Option<(usize, Duration)>)> {
     let reader = reader.with_guessed_format()?;
 
     // load animationed images
     if reader.format() == Some(ImageFormat::Gif) {
-        return decode_gif_image_from_reader(reader.into_inner(), frame_index)
-            .map(|(image, meta)| (image, Some(meta)));
+        let (image, meta) = decode_gif_image_from_reader(reader.into_inner(), frame_index)?;
+
+        return Ok((image, Some(meta)));
+    }
+
+    if reader.format() == Some(ImageFormat::Pnm) {
+        let image =
+            decode_pnm_image_from_reader(reader.into_inner(), foreground_color, background_color)?;
+
+        return Ok((image, None));
     }
 
     let image_result = reader.decode()?;
@@ -172,6 +227,12 @@ fn define_image(frame: LispFrameRef, img: *mut Emacs_Image, image_buffer: Dynami
     };
 }
 
+fn color_to_rgba(color: ColorF) -> Rgba<u8> {
+    let color: ColorU = color.into();
+
+    Rgba([color.r, color.g, color.b, color.a])
+}
+
 pub fn load_image(
     frame: LispFrameRef,
     img: *mut Emacs_Image,
@@ -182,7 +243,32 @@ pub fn load_image(
     let lisp_index = unsafe { Fplist_get(spec, QCindex) };
     let frame_index = lisp_index.as_fixnum().unwrap_or(0) as usize;
 
-    let loaded_image = open_image(spec_file, spec_data, frame_index);
+    let foreground_color = unsafe { Fplist_get(spec, QCforeground) };
+    let background_color = unsafe { Fplist_get(spec, QCbackground) };
+
+    let foreground_color = foreground_color
+        .as_string()
+        .and_then(|s| {
+            let s = s.to_string();
+            lookup_color_by_name_or_hex(&s)
+        })
+        .unwrap_or_else(|| pixel_to_color(unsafe { (*img).face_foreground }));
+
+    let background_color = background_color
+        .as_string()
+        .and_then(|s| {
+            let s = s.to_string();
+            lookup_color_by_name_or_hex(&s)
+        })
+        .unwrap_or_else(|| pixel_to_color(unsafe { (*img).face_background }));
+
+    let loaded_image = open_image(
+        spec_file,
+        spec_data,
+        frame_index,
+        color_to_rgba(foreground_color),
+        color_to_rgba(background_color),
+    );
 
     if loaded_image == None {
         let format_str = CString::new("Unable to load image %s").unwrap();
