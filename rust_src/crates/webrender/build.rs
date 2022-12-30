9794a436145d6f6feecbbf10f57b8474c2db3fd0
Declan Tsien <declantsien@riseup.net>
2022-12-30 10:57:21 +0800

    chore(build): Utilizing Cargo
    build: ng-bindgen use real crate name
    
    build: decouple hashdir

--- rust_src/crates/webrender/build.rs
+++ rust_src/crates/webrender/build.rs
@@ -3,9 +3,8 @@ extern crate ng_bindgen;
 use ng_bindgen::{generate_crate_exports, BuildError};
 
 fn main() {
-    for varname in ["EMACS_CFLAGS", "SRC_HASH"].iter() {
-        println!("cargo:rerun-if-env-changed={}", varname);
-    }
+    // TODO watch relevent files to re rerun, rs files under src?
+
     let path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     if let Err(e) = generate_crate_exports(&path) {
         match e {
